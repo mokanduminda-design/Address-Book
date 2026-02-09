597ce27c55604375951c1dcad78ff656371faf49
Mokandu Minda <mindamokandu6@gmail.com>
2026-02-09 12:22:39 +0300

    Add button handler , UI and DOM

--- js/script.js
+++ js/script.js
@@ -25,3 +25,46 @@ PlaceManager.prototype.deletePlace = function(id) {
     return false;
     };
 
+  function Place(location, timeOfYear, landmarks, notes) {
+    this.location = location;
+    this.timeOfYear = timeOfYear;
+    this.landmarks = landmarks
+    this.notes = notes;
+    this.id = null   // Given by generator
+}
+
+// UI & DOM manipulationm
+const myPlaces = new PlaceManager();
+
+// DOM Elements
+const listEl = document.getElementById(`placesList`)
+
+// HANDLER: Add Button Click
+   function handleAddPlace() {
+
+    const locIn = document.getElementById("location");
+    const timeIn = document.getElementById("timeOfYear");
+    const landIn = document.getElementById("landmarks");
+    const notesIn = document.getElementById("notes");
+
+    // 1. Create Object
+    const newPlace = new Place(
+        locIn.value,
+        timeIn.value,
+        landIn.value,
+        notesIn.value
+    );
+
+    // 2. Store Data
+    myPlaces.addPlace(newPlace);
+
+    // 3. Update UI
+    renderPlaces();
+
+    // 4. Reset Inputs
+    locIn.value = "";
+    timeIn.value = "";
+    landIn.value = "";
+    notesIn.value = "";
+}
+
